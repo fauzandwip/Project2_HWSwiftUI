cf1e96c98cf353fc5887bbbb424cb0888e23ebdd
fauzandp <fauzan27dp@gmail.com>
2023-06-01 13:12:20 +0700

    Project2_SwiftUI | Guess The Flag App add code to show alert while flag tapped

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
-    var correctAnswer = Int.random(in: 0...2)
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
     
     var body: some View {
         ZStack {
@@ -27,7 +30,7 @@ struct ContentView: View {
                 
                 ForEach(0..<3) { number in
                     Button {
-                        
+                        flaggedTapped(number)
                     } label: {
                         Image(countries[number])
                             .renderingMode(.original)
@@ -35,6 +38,26 @@ struct ContentView: View {
                 }
             }
         }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue") { askQuestion() }
+        } message: {
+            Text("Your score is ???")
+        }
+    }
+    
+    func flaggedTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Wrong"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
     
 }
