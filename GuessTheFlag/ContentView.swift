318809cb58987cff6d0e3045ef1fd57d7b3b188b
fauzandp <fauzan27dp@gmail.com>
2023-06-01 15:55:16 +0700

    Project2_SwiftUI | Guess The Flag App upgrade design upgrade background with RadialGradient font color, size, weight add material glass to main guess background add title and score manage the auto layout with spacer

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -16,31 +16,61 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     
     var body: some View {
+        
         ZStack {
-            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)], center: .top, startRadius: 200, endRadius: 700)
+            .ignoresSafeArea()
             
-            VStack(spacing: 30) {
-                VStack {
-                    Text("Tap the flag of")
-                        .foregroundColor(.white)
-                        .font(.subheadline.weight(.heavy))
-                    Text(countries[correctAnswer])
-                        .foregroundColor(.white)
-                        .font(.largeTitle.weight(.semibold))
-                }
+            // full body
+            VStack {
+                Spacer()
                 
-                ForEach(0..<3) { number in
-                    Button {
-                        flaggedTapped(number)
-                    } label: {
-                        Image(countries[number])
-                            .renderingMode(.original)
-                            .clipShape(Capsule())
-                            .shadow(radius: 5)
+                // title section
+                Text("Guess The Flag")
+                    .foregroundColor(.white)
+                    .font(.largeTitle.bold())
+                
+                // main guess flag
+                ZStack {
+                    VStack(spacing: 15) {
+                        VStack {
+                            Text("Tap the flag of")
+                                .foregroundStyle(.secondary)
+                                .font(.subheadline.weight(.heavy))
+                            Text(countries[correctAnswer])
+                                .font(.largeTitle.weight(.semibold))
+                        }
+                        
+                        ForEach(0..<3) { number in
+                            Button {
+                                flaggedTapped(number)
+                            } label: {
+                                Image(countries[number])
+                                    .renderingMode(.original)
+                                    .clipShape(Capsule())
+                                    .shadow(radius: 5)
+                            }
+                        }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.thinMaterial)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                // score section
+                Text("Score: ???")
+                    .foregroundColor(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
+            .padding()
         }
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue") { askQuestion() }
