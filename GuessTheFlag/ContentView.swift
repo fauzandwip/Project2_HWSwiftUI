73371a4c38e2e4a2b2efcb6f3c4933455e5f4817
fauzandp <fauzan27dp@gmail.com>
2023-06-01 16:24:57 +0700

    Project2_SwiftUI | Guess The Flag App Challenge 2 change alert while wrong answer

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -85,7 +85,7 @@ struct ContentView: View {
             scoreTitle = "Correct"
             score += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong!\n That's the flag of \(countries[number])"
         }
         
         showingScore = true
